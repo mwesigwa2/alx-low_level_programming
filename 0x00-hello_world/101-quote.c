64665cba6e6876eb7153050f62b68269efb1c37c
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-20 19:59:57 +0000

    print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - output to standard error
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
