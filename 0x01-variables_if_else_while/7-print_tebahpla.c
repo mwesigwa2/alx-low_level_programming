3ff51667ba1f59a6b2a8442922c0179b28889f33
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-21 02:45:45 +0000

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - SMile in the mirror
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description - Program prints lowercase alphabet in reverse using putchar()
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	       putchar(x);
+	putchar('\n');	
+	return (0);
+}
