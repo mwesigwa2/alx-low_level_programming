5cadecd9ce39ce6f35560405abfb7285362945a3
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-21 02:38:56 +0000

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  *
- * Description - Program that prints single digit 
+ * Description - Program that prints single digit
  * numbers of base 10 starting from 0, followed
  * by a new line. Using putchar
  */
