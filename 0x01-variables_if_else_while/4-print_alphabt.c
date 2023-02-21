9dd861d2d707735b177b20066b79f4dcc750f2e3
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-21 01:39:36 +0000

    exlcude letters from aplhabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 /**
- * main - When I was having that alphabet soup, I never thought that it would pay off
+ * main - When I was having that alphabet soup, 
+ * I never thought that it would pay off
  *
  * Return: Always 0 (Success)
  *
- * Description - program prints an alphabet with exception of letters q and e
+ * Description - program prints an alphabet with 
+ * exception of letters q and e
  */
 int main(void)
 {
 	char tt;
+	
 	tt = 'a';
 
 	while (tt <= 'z')
