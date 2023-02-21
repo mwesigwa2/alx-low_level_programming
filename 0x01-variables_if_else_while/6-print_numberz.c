abd33decb447927754925552a5a6059261c80769
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-21 02:37:31 +0000

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description - Program that prints single digit 
+ * numbers of base 10 starting from 0, followed
+ * by a new line. Using putchar
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar((x % 10) + '0');
+	putchar('\n');
+	return (0);
+}
