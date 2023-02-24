347914bdf9224b6ec54e6ed29a7848c780958990
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-24 20:31:28 +0000

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - causes an infinite loop
  * Return: 0
  */
@@ -8,13 +8,17 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	i = 0;
-	while (i < 10)
-	{
-		putchar(i);
-	}
+	/**
+	 * i = 0;
+	 *
+	 * while (i < 10)
+	 * {
+	 *	putchar(i);
+	 * }
+	 */
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Assign a random number to a variable
+ *
+ * Description - Use of if else clause
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+														       
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
