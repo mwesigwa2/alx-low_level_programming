b3aa343bde4d619933dc0287501bff47eeac2cd2
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-24 21:10:13 +0000

    Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,33 +1,24 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Assign a random number to a variable
+ * positive_or_negative - checks for negative and positive number
+ *@i: parameter number to enter
  *
- * Description - Use of if else clause
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	/* your code goes there */
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-														       
-	else if (n == 0)
+	if (i > 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
 	} else
 
 	{
-		printf("%d is negative\n", n);
+		printf("%d is zero\n", i);
 	}
-	return (0);
 }
