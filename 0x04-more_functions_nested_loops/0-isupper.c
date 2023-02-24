c093d3cf050abbee6c0e9ca3ab989a3847f3c14b
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-24 23:59:12 +0000

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - checks for the uppercase character
+ * @c: integer character to be checked
+ * Return: 1 if is uppercase otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c ==
+}
