f424dbed88da575027a520425d4a6cec3f8ae66a
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-02-25 08:26:56 +0000

    check uppercase character

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ *
+ * @c: Character to be checked
+ * Returns: 1 if c is a digit else 0
+ *
+ */
+int _isdigit(int c)
+{
+	int i, j;
+
+	for (i = 0; i <= 9, i++)
+	{
+		if (i
+	}
+}
