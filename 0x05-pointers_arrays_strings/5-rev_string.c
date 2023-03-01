e797fba46211d8d411e79d18091b07b3b751b449
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-03-01 01:48:22 +0000

    9. strcpy

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,40 +1,51 @@
 #include "main.h"
-#include <string.h>
-#include <unistd.h>
+
 /**
  * rev_string - function reverses a string
  * @s: pointer to string that is to be reversed
- * _putchar - function call to _putchar
+ *
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 void rev_string(char *s)
 {
-	int n, x;
-	char *begin, *end, tmp;
+	int x = 0, len;
 
-	while (s[n] != '\0')
+	len = _strlen(s) - 1;
+	while (len > x)
 	{
-		n++;
+		swap_char((s + len), (s + x));
+		x++;
+		len--;
 	}
 
-	begin = s;
-	end = s;
-	for (x = 0; x < n - 1; x++)
-		end++;
+}
 
-	for (x = 0; x < n / 2; x++)
+
+/**
+ *  * _strlen - function that returns the length of a string
+ *   * @s: string whose length is returned
+ *    * Return: 1
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		tmp = *end;
-		*end = *begin;
-		*begin = tmp;
-
-		begin++;
-		end--;
 	}
-	_putchar(s[x]);
-	_putchar('\n');
+	return (x);
+}
+
+/**
+ *  * swap_int - function that swaps values of two integers
+ *   * @a: integer pointer 1
+ *    * @b: integer pointer 2
+ *
+ */
+void swap_char(char *a, char *b)
+{
+	char x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
 }
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character starting with first
+ * character followed by new line
+ * @str: pointer to string
+ *
+ */
+void puts2(char *str)
+{
+	
+}
