7aed47f4faa8b6157ef3134b81dd14ccf6f671da
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-03-02 20:53:33 +0000

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+	{
+		while (!(s[x] >= 'a' && s[x] <= 'z'))
+		{
+			x++;
+		}
+		if (s[x - 1] == ' ' || s[x - 1] == ',' || s[x - 1] == ';' ||
+			s[x - 1] == '.' || s[x - 1] == '!' || s[x - 1] == '?' ||
+			s[x - 1] == '"' || s[x - 1] == '(' || s[x - 1] == ')' ||
+			s[x - 1] == '{' || s[x - 1] == '}' || s[x - 1] == '\t'|| s[x - 1] == '\n')
+		{
+			s[x] -= 32;
+		}
+		x++;
+	}
+	return (s);
+
+
+
+}
