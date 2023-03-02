2cbe5edd68891ad076985326a2677687043e36c2
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-03-02 21:55:57 +0000

    encode a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string
+ * @x: string to be encoded
+ * Return: s value
+ */
+char *leet(char *s)
+{
+	int x, y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (s[x] == s1[y])
+			{
+				s[x] = s2[y];
+			}
+
+		}
+	}
+	return (s);
+}
+
