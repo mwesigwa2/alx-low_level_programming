9775454029ec27b3c943abe1ddabe2359dc5a6b7
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-03-02 14:21:56 +0000

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,30 +8,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len;
+	int x;
 
-	len = 0;
-	while (*s1 != '\0' && *s2 != '\0')
+	for (x = 0; s1[x] != '\0' && s2[x] != '\0'; x++)
 	{
-		if (*s1 != *s2)
-		{
-			len = 1;
-			break;
-		}
-		s1++;
-		s2++;
+		if (s1[x] != s2[x])
+			return (s1[x] - s2[x]);
 	}
-
-	if (len == 0 && s1[len] == '\0' && s2[len] == '\0')
-		return (0);
-
-	if (*s1 < *s2)
-	{
-		return (-len);
-	}
-	else
-	{
-		return (len);
-	}
-
+	return (0);
 }
