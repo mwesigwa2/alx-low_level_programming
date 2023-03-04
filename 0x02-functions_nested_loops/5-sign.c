17cba5f72b0bb104a3df9d323a3bcc9e6c9b7308
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-03-04 18:59:31 +0000

    print sign of integer

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,6 +14,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
@@ -25,4 +26,5 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
