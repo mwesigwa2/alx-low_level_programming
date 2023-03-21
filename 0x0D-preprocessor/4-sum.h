c972872c0aa9b54edecf8a10633c3b252aedcdbb
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-03-21 02:26:45 +0000

    4.SUM commit

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: function-like macro SUM(x. y) that computes the sum of
+ * the numbers x and y.
+ *
+ */
+#define SUM(x, y) (x + y)
+#endif
