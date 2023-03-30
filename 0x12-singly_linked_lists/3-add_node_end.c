34aca008dfeb595c7195eac703e76462330e3513
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-03-30 21:15:44 +0000

    3.Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
 * add_node_end - function that adds a new node at end of list
 * @head: pointer to head of the linked list
@@ -7,7 +6,6 @@
 * Return: address of new element, or NULL if failed
 * str to be duplicated using strdup
 */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *current_node;
@@ -18,7 +16,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	/*Duplicate string*/
 	dup_str = strdup(str);
 	if (dup_str == NULL)
@@ -30,12 +27,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	/*get length of string*/
 	for (len = 0; str[len];)
 		len++;
-	
-	/* Update the new node to point to NULL*/
- 	new_node->str = dup_str;
-	new_node->len = len;
-	new_node->next = NULL;	
 
+	/* Update the new node to point to NULL*/
+	new_node->str = dup_str;
+	new_node->len = len;
+	new_node->next = NULL;
 	/* If the list is empty, make the new node the head*/
 	if (*head == NULL)
 	{
@@ -52,5 +48,4 @@ list_t *add_node_end(list_t **head, const char *str)
 		current_node->next = new_node;
 	}
 	return (*head);
-
 }
