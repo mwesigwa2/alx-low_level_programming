296dde5bf80492ba9c777a0a096c898eac2fcdc1
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-03-30 15:01:42 +0000

    correct formatting for print list function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-	int nodes = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
+
 			printf("[0] (nil)\n");
-		}
+
 		else
-		printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
+
 		nodes++;
 		h = h->next;
 	}
