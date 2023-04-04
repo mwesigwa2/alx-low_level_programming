b3ed32311ead4022db10efb9abf6c35a960f7b94
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-04-04 18:29:16 +0000

    2. Add node correction

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,9 +15,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-	ptr->n = n;
-	ptr->next = *head;
-	*head = ptr;
-
+	if (ptr == NULL)
+		return (NULL);
+	else
+	{
+		ptr->n = n;
+		ptr->next = *head;
+		*head = ptr;
+	}
 	return (ptr);
 }
