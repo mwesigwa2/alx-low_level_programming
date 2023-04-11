64d34d86f0b4584139f8e3bdf2f5005027fa5b10
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-04-11 08:44:02 +0000

    add case to handle NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,14 +7,14 @@
 * string b that is not 0 or 1, or when b is NULL.
 */
 
-
+/*
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int x = 0;
 	int y = 0;
 
 
-	if (b[y] == '\0')
+	if ((b[y] == '\0') || (b == NULL))
 		return (0);
 
 	while (b[y] != '\0')
@@ -30,6 +30,32 @@ unsigned int binary_to_uint(const char *b)
 		else
 			return (0);
 		y++;
+	}
+	return (x);
+}*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int k, j, x = 0, y = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[y] != '\0')
+		y++;
+
+	for (j = 0; b[j] != 0; j++)
+	{
+		k = (b[j] - '0');
+		if (k >= 2)
+		{
+			return (0);
+		}
+		else
+		{
+			x += (k * (1 << (y - 1)));
+			y--;
+		}
 	}
 	return (x);
 }
