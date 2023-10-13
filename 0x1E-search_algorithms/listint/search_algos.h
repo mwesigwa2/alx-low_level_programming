8084b3f0438d6c05b91cf2ea886bd0b8c9ccb8f7
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-10-13 02:40:26 +0000

    function that searches for a value in a sorted list of integers using the Jump search algorithm.

--- 0x1E-search_algorithms/listint/search_algos.h
+++ 0x1E-search_algorithms/listint/search_algos.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <math.h>
 
 /**
  * struct listint_s - singly linked list
@@ -45,4 +44,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 #endif
