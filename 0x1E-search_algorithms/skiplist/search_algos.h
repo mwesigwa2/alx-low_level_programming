75f8d95caf7cdf147fa6ac8a56996c99be08dfcf
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-10-13 02:54:58 +0000

    function that searches for a value in a sorted skip list of integers.

--- 0x1E-search_algorithms/skiplist/search_algos.h
+++ 0x1E-search_algorithms/skiplist/search_algos.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <math.h>
 
 /**
  * struct listint_s - singly linked list
@@ -46,4 +45,8 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
+void init_express(skiplist_t *list, size_t size);
+void free_skiplist(skiplist_t *list);
+void print_skiplist(const skiplist_t *list);
 #endif
